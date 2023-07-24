9eac4db9ffb8b0c9a9fc9d920879d5bc04d7188a
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2023-07-24 14:57:17 +0700

    membuat funngsi menghitung luas dan keliling segitiga

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // input untuk pengguna/ untuk pengguna memasukkan jari-jari lingkaran
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan jari-jari lingkaran: ");
+        float radius = scanner.nextFloat();
+
+        // menghitung luas & keliling lingkaran dengan fungsi hitungLuasLingkaran & hitungKelilingLingkaran dari lingkaran
+        double luas = hitungLuasLingkaran(radius);
+        double keliling = hitungKelilingLingkaran(radius);
+
+        // menampilkan untuk hasil perhitungan luas dan keliling lingkaran dengan format 2 angka dibelakang koma dengan print format
+        System.out.printf("Luas lingkaran: %.2f%n", luas);
+        System.out.printf("Keliling lingkaran: %.2f%n", keliling);
+    }
+
+    // fungsi untuk menghitung luas lingkaran
+    public static double hitungLuasLingkaran(float radius) {
+        return Math.PI * radius * radius;
+    }
+
+    // fungsi untuk menghitung keliling lingkaran
+    public static double hitungKelilingLingkaran(float radius) {
+        return 2 * Math.PI * radius;
+    }
+}
